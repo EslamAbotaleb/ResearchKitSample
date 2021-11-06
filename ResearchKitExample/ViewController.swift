ccbb3152a402f4d6c92f049d290944a6f106632f
eslamabotalebdev <eslamabotalebdev@gmail.com>
2021-11-06 23:20:41 +0200

    Initial Commit

---
+++ ResearchKitExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResearchKitExample
+//
+//  Created by Eslam Abotaleb on 06/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
